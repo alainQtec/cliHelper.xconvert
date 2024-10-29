664dc917f3975818070c05d7933e034682327b10
Alain Herve <alainQtec@gmail.com>
2024-10-29 23:22:05 +0200

    Update Invoke-Converter.ps1

--- Public/Invoke-Converter.ps1
+++ Public/Invoke-Converter.ps1
@@ -1,39 +1,74 @@
 function Invoke-Converter {
   # .SYNOPSIS
-  #   Creates a new [xconvert] object
+  #  Convert objects
   # .DESCRIPTION
-  #   Creates a custom Converter object.
+  #  Creates a custom Converter object and Invokes methods on it.
   # .EXAMPLE
-  #  $enc_Pass = "HelloWorld" | xconvert -m ToBase32String, ToObfuscated, ToSecurestring
-  #  $txt_Pass = $enc_Pass | xconvert -m ToString, FromObfuscated, FromBase32String, ToInt32, Tostring
+  #  $enc_Pass = "HelloWorld" | Xconvert ToBase32String, ToObfuscated, ToSecurestring
+  #  $txt_Pass = $enc_Pass | xconvert ToString, FromObfuscated, FromBase32String, ToInt32, Tostring
   #  $txt_Pass | Should -Be "HelloWorld"
-  #  Thats chaining methods
+  # Thats chaining methods
   [CmdletBinding()]
   [Alias('xconvert')]
-  [OutputType([xconvert], [object], [object[]])]
-  param (
-    [Parameter(Position = 0, ValueFromPipeline = $true)]
-    [Alias('i')][ValidateNotNullOrEmpty()]
-    $object,
-
-    [Parameter(Position = 1)]
+  [OutputType({ [xconvert]::ReturnTypes })]
+  param(
+    [Parameter(Position = 0)]
     [Alias('m')][ValidateNotNullOrEmpty()]
-    [string[]]$Method
+    [ArgumentCompleter({
+        [OutputType([System.Management.Automation.CompletionResult])]
+        param(
+          [string] $CommandName,
+          [string] $ParameterName,
+          [string] $WordToComplete,
+          [System.Management.Automation.Language.CommandAst] $CommandAst,
+          [System.Collections.IDictionary] $FakeBoundParameters
+        )
+        $CompletionResults = [System.Collections.Generic.List[System.Management.Automation.CompletionResult]]::new()
+        $staticMethods = [xconvert].GetMethods().Where({ $_.IsStatic -and !$_.IsHideBySig }) | Sort-Object -Unique Name
+        $matchingMethods = $staticMethods | Where-Object { $_.Name -like "$WordToComplete*" }
+        foreach ($method in $matchingMethods) {
+          $paramst = ($method.GetParameters() | Select-Object @{l = '_'; e = { "[$($_.ParameterType.Name)]`$$($_.Name)" } })._ -join ', '
+          $toolTip = "{0}({1}) -> {2}" -f $method.Name, $paramst, $method.ReturnType.Name
+          $completionResult = [System.Management.Automation.CompletionResult]::new(
+            $method.Name, # CompletionText
+            $method.Name, # ListItemText
+            'Method',
+            $toolTip
+          )
+          $CompletionResults.Add($completionResult)
+        }
+        return $CompletionResults
+      })]
+    [string[]]$Method,
+
+    [Parameter(Position = 1, ValueFromPipeline = $true)]
+    [Alias('i')][ValidateNotNullOrEmpty()]
+    $object
   )
+
   begin {
+    if ($PSBoundParameters.ContainsKey("Method") -and !$PSBoundParameters.ContainsKey("object")) {
+      $PSCmdlet.ThrowTerminatingError(
+        [System.Management.Automation.ErrorRecord]::new(
+          [System.ArgumentNullException]::new("object", "You must supply a value for -object when using -Method"),
+          "Parameter cannot be null or empty",
+          "InvalidArgument",
+          $null
+        )
+      )
+    }
     $c = [xconvert]::new()
   }
+
   process {
     if ($object) {
       $r = $object
-      $Method.ForEach({
-          $r = $c::$_($r)
-        }
-      )
+      $Method.ForEach({ $r = $c::$_($r) })
     } else {
       $r = $c
     }
   }
+
   end {
     return $r
   }
