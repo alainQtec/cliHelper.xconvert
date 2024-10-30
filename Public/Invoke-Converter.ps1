a65e39504142458971d9c6c54bef86f4d0cfb6fc
Alain Herve <admin@alainQtec.dev>
2024-10-30 21:29:34 +0200

    Update Invoke-Converter.ps1

--- Public/Invoke-Converter.ps1
+++ Public/Invoke-Converter.ps1
@@ -1,3 +1,4 @@
+using namespace System.Management.Automation
 function Invoke-Converter {
   # .SYNOPSIS
   #  Convert objects
@@ -15,20 +16,20 @@ function Invoke-Converter {
     [Parameter(Mandatory = $false, Position = 0)]
     [Alias('m')][ValidateNotNullOrEmpty()]
     [ArgumentCompleter({
-        [OutputType([System.Management.Automation.CompletionResult])]
+        [OutputType([CompletionResult])]
         param(
           [string] $CommandName,
           [string] $ParameterName,
           [string] $WordToComplete,
-          [System.Management.Automation.Language.CommandAst] $CommandAst,
+          [Language.CommandAst] $CommandAst,
           [System.Collections.IDictionary] $FakeBoundParameters
         )
-        $CompletionResults = [System.Collections.Generic.List[System.Management.Automation.CompletionResult]]::new()
+        $CompletionResults = [System.Collections.Generic.List[CompletionResult]]::new()
         $matchingMethods = [xconvert]::Methods.Where({ $_.Name -like "$WordToComplete*" })
         foreach ($method in $matchingMethods) {
           $paramst = ($method.GetParameters() | Select-Object @{l = '_'; e = { "[$($_.ParameterType.Name)]`$$($_.Name)" } })._ -join ', '
           $toolTip = "{0}({1}) -> {2}" -f $method.Name, $paramst, $method.ReturnType.Name
-          $completionResult = [System.Management.Automation.CompletionResult]::new(
+          $completionResult = [CompletionResult]::new(
             $method.Name, # CompletionText
             $method.Name, # ListItemText
             'Method',
@@ -50,9 +51,9 @@ function Invoke-Converter {
   process {
     $InvalidMethods = $Method.Where({ $_ -notin [xconvert]::Methods.Name })
     if ($InvalidMethods.Count -gt 0) {
-      $PSCmdlet.ThrowTerminatingError([System.Management.Automation.ErrorRecord]::new(
-          [System.InvalidOperationException]::new("Please use valid method names",
-            [System.Management.Automation.MethodInvocationException]::new("Methods ($($InvalidMethods -join ', ')) not found")),
+      $PSCmdlet.ThrowTerminatingError([ErrorRecord]::new(
+          [System.InvalidOperationException]::new("Please use valid method names. Methods ($($InvalidMethods -join ', ')) were not found.",
+            [MethodInvocationException]::new("")),
           "METHOD_NOT_FOUND",
           "InvalidArgument",
           $null
@@ -62,7 +63,7 @@ function Invoke-Converter {
     # $PSBoundParameters.GetEnumerator() | ForEach-Object { Write-Debug "$($_.Key) = $($_.Value)" -Debug }
     if ($PSBoundParameters.ContainsKey("Method") -and !$PSBoundParameters.ContainsKey("object")) {
       $PSCmdlet.ThrowTerminatingError(
-        [System.Management.Automation.ErrorRecord]::new(
+        [ErrorRecord]::new(
           [System.ArgumentNullException]::new("Object", "You must supply a value for -object when using -Method"),
           "Parameter cannot be null or empty",
           "InvalidArgument",
